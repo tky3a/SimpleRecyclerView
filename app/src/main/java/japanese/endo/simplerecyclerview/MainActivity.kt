de0a867f628cf5c3b81270f316871440895480dc
tky <takayamac@icloud.com>
2023-04-11 22:22:33 +0900

    Initial commit

---
+++ app/src/main/java/japanese/endo/simplerecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package japanese.endo.simplerecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
